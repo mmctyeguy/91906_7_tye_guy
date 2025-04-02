2e1e24ce48e33f85d0bee865ce447eab373bc534
mmctyeguy <tyeguy@mmc.school.nz>
2025-04-02 17:23:39 +1300

    basic GUI and bet amount handling

---
+++ 01_game_gui_v1.py
@@ -0,0 +1,100 @@
+from tkinter import *
+
+
+class StartGame:
+    """
+    Initial game interface (asks users the bet amount)
+    """
+
+    def __init__(self):
+        """
+        gets bet amount from user
+        """
+
+        self.start_frame = Frame(padx=10, pady=10)
+        self.start_frame.grid()
+
+        # strings for labels
+        intro_string = ("Welcome to Blackjack!")
+
+        bet_string = "How much do you want to bet?"
+
+        # List of labels to be made (text | font | fg)
+        start_labels_list = [
+            ["Blackjack", ("Arial", "16", "bold"), None],
+            [intro_string, ("Arial", "12"), None],
+            [bet_string, ("Arial", "12", "bold"), "#009900"],
+
+        ]
+
+        # Create labels and add them to reference list.
+
+        start_label_ref = []
+        for count, item in enumerate(start_labels_list):
+            make_label = Label(self.start_frame, text=item[0], font=item[1],
+                               fg=item[2],
+                               wraplength=350, justify="left", pady=10, padx=20)
+            make_label.grid(row=count)
+
+            start_label_ref.append(make_label)
+
+        # extract choice label so that it can be changed to an
+        # error message if necessary.
+        self.choose_label = start_label_ref[2]
+
+        # Frame so that entry box and button can be in the same row
+        self.entry_area_frame = Frame(self.start_frame)
+        self.entry_area_frame.grid(row=3)
+
+        # create round enter
+        self.bet_entry = Entry(self.entry_area_frame, font=("Arial", "28", "bold"), width=10)
+        self.bet_entry.grid(row=2, column=0, padx=10, pady=30)
+
+        # create play button...
+        self.play_button = Button(self.entry_area_frame, font=("Arial", "16", "bold"),
+                                  fg="#FFFFFF", bg="#0057D8", text="Play",
+                                  width=10, command=self.check_bet)
+        self.play_button.grid(row=4, column=0)
+
+    def check_bet(self):
+
+        bet_amount = self.bet_entry.get()
+
+        self.choose_label.config(fg="#009900", font=("Arial", "12", "bold"))
+        self.bet_entry.config(bg="#FFFFFF")
+
+        error = "Oops - Please choose a number more than zero."
+        has_errors = "no"
+
+        # checks that the amount of rounds is above 0
+        try:
+            bet_amount = float(bet_amount)
+            if bet_amount > 0:
+                # clear entry box and reset instruction label so
+                # that when users play a new game, they don't see an error message
+                self.bet_entry.delete(0, END)
+                self.choose_label.config(text="How much would you like to bet?")
+                # invoke play class (and take across bet amount)
+                # PLACEHOLDER
+                # hide root window
+                root.withdraw()
+            else:
+                has_errors = "yes"
+
+        except ValueError:
+            has_errors = "yes"
+
+        # display error if necessary
+        if has_errors == "yes":
+            self.choose_label.config(text=error, fg="#990000",
+                                     font=("Arial", "10", "bold"))
+            self.bet_entry.config(bg="#F4CCCC")
+            self.bet_entry.delete(0, END)
+
+
+# main routine
+if __name__ == "__main__":
+    root = Tk()
+    root.title("Black Jack")
+    StartGame()
+    root.mainloop()
